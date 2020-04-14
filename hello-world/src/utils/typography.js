b841279aa36af10a2311fb9076304ba9683add85
Rosalie Chan <rosaliemchan@yahoo.com>
2020-04-13 23:16:52 -0700

    Updates

--- hello-world/src/utils/typography.js
+++
@@ -1,8 +0,0 @@
-import Typography from "typography"
-import sternGroveTheme from "typography-theme-stern-grove"
-
-const typography = new Typography(sternGroveTheme)
-
-export const { scale, options } = typography
-export default typography
-export const rhythm = typography.rhythm
